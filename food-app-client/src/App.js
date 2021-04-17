28c27d38b4a1e32b42b868c25a1695b237d40a89
catherinesmiley <catherinesmiley@gmail.com>
2021-04-17 14:51:40 -0400

    create home component and mount in app

--- food-app-client/src/App.js
+++ food-app-client/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 
+import Home from './components/Home'
+
 class App extends Component {
     render() {
         return (
-            <div>Hi!!!!!!!!!!</div>
+            <div>
+                <Home />
+            </div>
         )
     }
 }
