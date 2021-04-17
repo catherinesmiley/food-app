9da9555412675167bd3c5ca7b1050b3aa2e9ece3
catherinesmiley <catherinesmiley@gmail.com>
2021-04-16 21:31:42 -0400

    install and set up store in index js file

--- food-app-client/src/index.js
+++ food-app-client/src/index.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { applyMiddleware, compose, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import App from './App'
+import { rootReducer } from './reducers/rootReducer'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(
+    rootReducer, composeWithDevTools(applyMiddleware(thunk))
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root')
+)
