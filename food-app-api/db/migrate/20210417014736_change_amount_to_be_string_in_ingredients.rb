d2756d28d1230af1da88dea8f135f768c9f055c9
catherinesmiley <catherinesmiley@gmail.com>
2021-04-16 21:48:42 -0400

    change amount in ingredients table to string

---
+++ food-app-api/db/migrate/20210417014736_change_amount_to_be_string_in_ingredients.rb
@@ -0,0 +1,5 @@
+class ChangeAmountToBeStringInIngredients < ActiveRecord::Migration[6.1]
+  def change
+    change_column :ingredients, :amount, :string
+  end
+end
--- food-app-api/db/schema.rb
+++ food-app-api/db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_17_012436) do
+ActiveRecord::Schema.define(version: 2021_04_17_014736) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
-    t.integer "amount"
+    t.string "amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "meal_id", null: false
