b67e59f7862cd914512a72476fe0a57971e88a46
catherinesmiley <catherinesmiley@gmail.com>
2021-04-18 13:01:59 -0400

    import router, route, and switch to app component

--- food-app-client/src/App.js
+++ food-app-client/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Home from './components/Home'
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <Home />
-            </div>
+            <Router>
+                <div>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                    </Switch>
+                </div>
+            </Router>
         )
     }
 }
