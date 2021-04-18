ab6113f4c81e5fef4a0fde7765ddc390055880c1
catherinesmiley <catherinesmiley@gmail.com>
2021-04-18 18:59:04 -0400

    create fetch ingredients action

---
+++ food-app-client/src/actions/ingredients.js
@@ -0,0 +1,9 @@
+const BASE_URL = "http://localhost:3000/ingredients"
+
+export const fetchIngredients = () => {
+    return dispatch => {
+        fetch(`${BASE_URL}`)
+        .then(resp => resp.json())
+        .then(ingredients => dispatch({ type: 'FETCH"INGREDIENTS', ingredients }))
+    }
+}
