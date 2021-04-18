39e4fac3ce9041976530ce82fa42a452edbe46b1
catherinesmiley <catherinesmiley@gmail.com>
2021-04-18 18:54:59 -0400

    create ingredients reducer and add to root reducer

--- food-app-client/src/reducers/rootReducer.js
+++ food-app-client/src/reducers/rootReducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 
 import { pantryItemsReducer } from './pantryItemsReducer'
 import { mealsReducer } from './mealsReducer'
+import { ingredientsReducer } from './ingredientsReducer'
 
 export const rootReducer = combineReducers({
     items: pantryItemsReducer,
-    meals: mealsReducer
+    meals: mealsReducer, 
+    ingredients: ingredientsReducer
 })
