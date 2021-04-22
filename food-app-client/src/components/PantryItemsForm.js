74eb21f04af5794072e2637d32fb3a489c50b854
catherinesmiley <catherinesmiley@gmail.com>
2021-04-21 21:02:25 -0400

    create pantry items form component

---
+++ food-app-client/src/components/PantryItemsForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PantryItemsForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default PantryItemsForm;
