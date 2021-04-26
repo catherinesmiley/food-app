28cbded6e71f47ff4963606cf387e2f9fc49cb1b
catherinesmiley <catherinesmiley@gmail.com>
2021-04-26 18:45:12 -0400

    add local state to meals form and create handle on change function

--- food-app-client/src/components/MealsForm.js
+++ food-app-client/src/components/MealsForm.js
@@ -5,12 +5,25 @@ import { addMeal } from '../actions/meals'
 
 class MealsForm extends Component {
 
+    state = {
+        title: '',
+        description: '',
+        instructions: ''
+    }
+
+    handleOnChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
     handleOnSubmit = event => {
         event.preventDefault()
         console.log("event submitted!")
     }
 
     render() {
+
         return (
             <div>
                 <form onSubmit={event => this.handleOnSubmit(event)}>
@@ -18,17 +31,23 @@ class MealsForm extends Component {
                         type="text"
                         placeholder="Meal title" 
                         name="title"
+                        onChange={this.handleOnChange}
+                        value={this.state.title}
                     />
                     <br />
                     <input
                         type="text"
                         placeholder="Meal description" 
                         name="description"
+                        onChange={this.handleOnChange}
+                        value={this.state.description}
                     />
                     <br />
                     <textarea
                         placeholder="Instructions" 
                         name="instructions"
+                        onChange={this.handleOnChange}
+                        value={this.state.instructions}
                     />
                     <br />
                     <input type="submit" value="Add new meal"/>
