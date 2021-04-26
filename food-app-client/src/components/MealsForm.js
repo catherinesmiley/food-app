4eb027d31b34e350b294dae965a8c03226838673
catherinesmiley <catherinesmiley@gmail.com>
2021-04-26 18:42:57 -0400

    add inputs and textarea to meals form and create handle on submit function

--- food-app-client/src/components/MealsForm.js
+++ food-app-client/src/components/MealsForm.js
@@ -4,10 +4,35 @@ import { connect } from 'react-redux'
 import { addMeal } from '../actions/meals'
 
 class MealsForm extends Component {
+
+    handleOnSubmit = event => {
+        event.preventDefault()
+        console.log("event submitted!")
+    }
+
     render() {
         return (
             <div>
-                Meals Form
+                <form onSubmit={event => this.handleOnSubmit(event)}>
+                    <input
+                        type="text"
+                        placeholder="Meal title" 
+                        name="title"
+                    />
+                    <br />
+                    <input
+                        type="text"
+                        placeholder="Meal description" 
+                        name="description"
+                    />
+                    <br />
+                    <textarea
+                        placeholder="Instructions" 
+                        name="instructions"
+                    />
+                    <br />
+                    <input type="submit" value="Add new meal"/>
+                </form>
             </div>
         );
     }
