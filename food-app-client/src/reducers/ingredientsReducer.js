683d001c0580e5df8936de20af51fca5cbfa1ede
catherinesmiley <catherinesmiley@gmail.com>
2021-05-09 21:02:04 -0400

    create add ingredient case to ingredients reducer

--- food-app-client/src/reducers/ingredientsReducer.js
+++ food-app-client/src/reducers/ingredientsReducer.js
@@ -2,6 +2,8 @@ export const ingredientsReducer = (state = [], action) => {
     switch (action.type) {
         case 'FETCH_INGREDIENTS': 
             return action.ingredients 
+        case 'ADD_INGREDIENT':
+            return [...state, action.ingredient]
         default: 
             return state 
         }
